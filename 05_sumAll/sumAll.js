4b3754e735e5338c54529055dde771337c75d3f2
werdnales <werdnales@gmail.com>
2023-03-28 15:41:25 +0300

    Solve 'sumAll' exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function (first, second) {
+  // Find incorrect input:
+  if (
+    typeof first !== "number" ||
+    first < 0 ||
+    typeof second !== "number" ||
+    second < 0
+  ) {
+    return "ERROR";
+  }
 
+  // Find max:
+  if (first > second) {
+    const temp = second;
+    second = first;
+    first = temp;
+  }
+
+  let sum = first;
+  const length = second - first;
+
+  for (let i = 0; i < length; i++) {
+    sum += ++first;
+  }
+  return sum;
 };
 
 // Do not edit below this line
