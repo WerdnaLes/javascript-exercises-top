90bfb6153080feb3464abe2c70d58b83e7e3ce3d
werdnales <werdnales@gmail.com>
2023-03-28 15:03:00 +0300

    Solve 'removeFromArray' exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...num) {
+  // Longer variant:
+  //   let newArr = array;
 
+  //   for (let i = 0; i < array.length; i++) {
+  //     for (const rm of num) {
+  //       if (array[i] === rm) {
+  //         newArr.splice(i, 1);
+  //         i--;
+  //       }
+  //     }
+  //   }
+  //   return newArr;
+
+  // Much shorter variant
+  return array.filter((val) => !num.includes(val));
 };
 
 // Do not edit below this line
