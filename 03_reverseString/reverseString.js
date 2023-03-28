f574fd1619947eccf25b5bd00b3b194045b7ae77
werdnales <werdnales@gmail.com>
2023-03-28 14:20:47 +0300

    Solve 'reverseString' exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function (str) {
+  //   let res = "";
+  //   for (let i = str.length - 1; i >= 0; i--) {
+  //     res += str.charAt(i);
+  //   }
+  //   return res;
 
+  // OR better:
+  return str.split("").reverse().join("");
 };
 
 // Do not edit below this line
