1214a38d8febd1c6a2af2379bf12370f4424761b
werdnales <werdnales@gmail.com>
2023-04-05 18:32:29 +0300

    Solver exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let alphanumericOnly = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+  let reversedString = alphanumericOnly.split("").reverse().join("");
 
+  return reversedString === alphanumericOnly;
 };
+
+console.log(palindromes("Animal loots foliated detail of stool lamina."));
 
 // Do not edit below this line
 module.exports = palindromes;
