59df728abc1a62589f701ddd107225e5438e9f3c
werdnales <werdnales@gmail.com>
2023-04-05 16:05:07 +0300

    Solve most of 08 exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,34 @@
-const add = function() {
-	
+const add = function () {
+  let sum = 0;
+  let args = [...arguments];
+  args.forEach((element) => {
+    sum += element;
+  });
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function () {
+  let res = arguments[0];
+
+  for (let i = 1; i < arguments.length; i++) {
+    res -= arguments[i];
+  }
+  return res;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return array.reduce((init, el) => init + el, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  return array.reduce((init, el) => init * el);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
-};
+const factorial = function () {};
 
 // Do not edit below this line
 module.exports = {
@@ -29,5 +37,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
