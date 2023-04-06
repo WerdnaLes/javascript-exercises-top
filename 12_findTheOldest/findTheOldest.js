e64e1fceae093ec026fc2e743b48dbae5b4eb805
werdnales <werdnales@gmail.com>
2023-04-06 15:09:57 +0300

    Solve exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+  const modifiedArray = array
+    .map((element) => {
+      if (!element.yearOfDeath) {
+        element.yearOfDeath = new Date().getFullYear();
+      }
+      return element;
+    })
+    .sort((a, b) => {
+      return a.yearOfDeath - a.yearOfBirth > b.yearOfDeath - b.yearOfBirth
+        ? 1
+        : -1;
+    });
 
+  return modifiedArray.pop();
 };
 
 // Do not edit below this line
